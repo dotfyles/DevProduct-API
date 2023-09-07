a0f80328ead8a582785d86b8c925b5a1723327ec
dotfyles <136743188+dotfyles@users.noreply.github.com>
2023-09-07 01:38:57 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,55 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return '🚀 Api-Online'
 
-@app.route('/about')
-def about():
-    return 'About'
+def update(cookie, universeID, productID, price):
+	s = requests.Session()
+	def getCSRF():
+		c = s.post(
+			"https://auth.roblox.com/v2/signup"
+			)
+		return c.headers['x-csrf-token']
+
+	s.cookies[".ROBLOSECURITY"] = cookie
+	# r = s.post(
+	# 	"https://apis.roblox.com/developer-products/v1/universes/5068470209/developerproducts?name=ANOTHERON3ETS&description=hello&priceInRobux=100",
+	# 	headers={"Content-Type": "application/json", "X-Csrf-Token": getCSRF()}
+	# 	).text
+
+	r = s.post(
+			f"https://apis.roblox.com/developer-products/v1/universes/{str(universeID)}/developerproducts/{str(productID)}/update",
+			json={"PriceInRobux": price},
+			headers={"Content-Type": "application/json; charset=utf-8", "X-Csrf-Token": getCSRF()}
+		).text
+	return r
+
+@app.route('/api/developer', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        
+        cookie = json["cookie"]
+        universeID = json["universeID"]
+        productID = json["productID"]
+        price = json["price"]
+
+        print(update(cookie, universeID, productID, price))
+
+        if "{}" in update(cookie, universeID, productID, price):
+        	return jsonify({
+        			"Status": "Completed",
+        			"Updated Price": price
+        		})
+        else:
+        	return jsonify({
+        			"Status": "Failed"
+        		})
+
+    else:
+        return 'Content-Type not supported!'
